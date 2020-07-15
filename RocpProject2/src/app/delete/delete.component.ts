127f74cd916e063deca5d08642cdbc08423d288a
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2020-07-15 15:51:15 -0400

    bootsrap version

---
+++ RocpProject2/src/app/delete/delete.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ROCPService } from '../services/rocp.service';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
