8202d4d560e1e9f7e733643b907c26ccfec592f0
Mini Varghese <mvarghese03@gmail.com>
2020-07-14 23:47:27 -0500

    Added GetTaskByID

--- RocpProject2/src/app/profile/profile.component.ts
+++ RocpProject2/src/app/profile/profile.component.ts
@@ -1,42 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ROCPService } from '../services/rocp.service';
-import { FormGroup, FormControl } from '@angular/forms';
-import { stringify } from '@angular/compiler/src/util';
+import {ROCPService} from '../services/rocp.service';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit 
+{
+
+  attrtodosId = '';
+
+  get todosId(): string {
+    return this.attrtodosId;
+  }
+
+  set todosId(temp: string) {
+      this.attrtodosId = temp;
+     
+  }
 
   currentHero = 'No Hero';
-
+  // I am making an object version of my form that I WILL pair with the form I will create on the HTML page.
+  //This is using the reactive Forms module
   todos = new FormGroup({
     title: new FormControl('')
-  });
+    });
+
+    todosById = new FormGroup({
+      titleById: new FormControl('')})
+        
 
   constructor(private route: ActivatedRoute, private rocp: ROCPService) { }
 
   postTodoEc2(todoSub: FormGroup) {
     let form = JSON.stringify(todoSub.value);
+    console.log(form);
+    console.log(todoSub);
     this.rocp.postTodo(form).subscribe(
-      response => {
+      response=> {
         console.log('success');
-      }
 
-    );
+      }
+    )
 
   }
+
 
   getTodosEc2() {
     this.rocp.getTodos().subscribe(
       response => {
         console.log(response);
+       
       }
-    );
+    )
   }
+
+  
+  getTodoEc2ById(todosId: string) {
+    console.log(todosId);
+    this.rocp.getTodosByID(todosId).subscribe(
+     response => {
+        console.log(response);
+        })
+      }
 
   ngOnInit(): void {
     this.currentHero = this.route.snapshot.paramMap.get('heroname');
