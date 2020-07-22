483b9ff0d8848145d048ca3d1f64d0c1135db1e9
Mini Varghese <mvarghese03@gmail.com>
2020-07-22 13:37:46 -0500

    Fixed error handler and message display on create component and modified fieldset element

--- RocpProject2/src/app/create/create.component.ts
+++ RocpProject2/src/app/create/create.component.ts
@@ -9,24 +9,48 @@ import {FormGroup, FormControl} from '@angular/forms';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
+  statusCode: number;
+  data:any[];
+  taskInput: string;
+ 
   constructor(private route: ActivatedRoute, private rocp: ROCPService) { }
   todos = new FormGroup({
     completed: new FormControl(''),
     title: new FormControl('')
-   
-    
-    });
-  postTodoEc2(todoSub: FormGroup) {
-    let form = JSON.stringify(todoSub.value);
-    console.log('form ' + form);
-    console.log(todoSub);
-    this.rocp.postTodo(form).subscribe(
-      response => {
-        console.log('success');
+   });
 
-      }
-    );
+   clear(){
+     console.log("task input" + this.taskInput)
+    this.taskInput = '';
+    }
+
+  postTodoEc2(todoSub: FormGroup) {
+    const titleValue = todoSub.get('title').value;
+    if (titleValue == '' || titleValue == null)
+      {alert ('Please enter a valid Task Title')
+        this.statusCode = 0;}
+    else {
+      let form = JSON.stringify(todoSub.value);
+      console.log('form ' + form);
+      console.log(todoSub);
+      this.rocp.postTodo(form).subscribe(
+        response => {
+          console.log('success');
+          this.statusCode = 200;
+          this.rocp.getTodos().subscribe(
+            response1 => {
+              this.data = response1;
+             // this.statusCode = 200;
+              },
+              errorCode => this.statusCode = errorCode.status
+          );
+           },
+           errorCode => this.statusCode = errorCode.status
+
+      );
+      
+
+    }
 
   }
   ngOnInit(): void {
